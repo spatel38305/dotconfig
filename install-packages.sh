44d08d1f9dfde32206cdece8ac404f19db5bc33d
spatel38305 <spatel84@vols.utk.edu>
2021-11-11 05:00:41 -0500

    Updated install-packages script to print packages version and check for zsh.

--- install-packages.sh
+++ install-packages.sh
@@ -18,11 +18,19 @@ else
     echo "I can apt!"
 fi
 
+packagesVersion="$( cat "$DIR/packages/.spatel.packages" | xargs )"
+echo "packages version: $packagesVersion"
+
 packages="$( cat packages.txt | sort -u | xargs )"
 
 echo "Installing: $packages"
 
 xargs -0 -n 1 sudo apt install -y < <( tr \\n \\0 < packages.txt )
+
+if [ -n "$ZSH_VERSION" ]; then
+    echo "Shell is already zsh! Yay!"
+    exit
+fi
 
 if ! command -v zsh &> /dev/null; then
     echo "No zsh. Rip. :("
